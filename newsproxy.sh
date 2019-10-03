d4d4663aff9fb7351ac397ace8fecd8e55d8a530
Fionn Fitzmaurice <fionn@github.com>
2019-10-03 22:55:25 +0800

    Rewrite HTTP/2 responses as HTTP1.1
    Some platforms don't route HTTP/2, but some providers have trouble with
    HTTP/1.1 requests. This changes the proxied request to use HTTP/whatever and
    rewrites the response to always be HTTP/1.1.
    
    It also drops some unnecessary echo calls, introduces a logging
    function, a HTTP status checker and logic to return a 502 if it
    encounters anything but OK.

--- newsproxy.sh
+++ newsproxy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-set -eo pipefail
+set -eEo pipefail
 
 [ -z "$PORT" ] && readonly PORT=1234
 
 set -u
 
+function log {
+    echo -e "$1" >&2
+}
+
 function handler {
     local line
-    while read -r line
-    do
-        line=$(echo "$line" | tr -d "\r\n")
-        echo ">: $line" >&2
-        if echo "$line" | grep -qE "^GET /"
-        then
-            request=$(echo "$line" | cut -d " " -f2)
-        elif [ -z "$line" ]
-        then
+    while read -r line; do
+        line=$(tr -d "\r\n" <<< "$line")
+        log "> \e[2m$line\e[0m"
+        if grep -qE "^GET /" <<< "$line"; then
+            request=$(cut -d " " -f 2 <<< "$line")
+        elif [ -z "$line" ]; then
             proxy_request "$request"
             break
         fi
@@ -24,9 +25,26 @@ function handler {
 }
 
 function proxy_request {
-    local request="$DESTINATION_HOST""$1"
-    echo "Proxying request for $request" >&2
-    curl --http1.1 -ie "$REFERRER" "https://$request"
+    local request="https://$DESTINATION_HOST""$1"
+
+    log "Proxying request for $request"
+    out=$(curl -sSie "$REFERRER" "$request")
+
+    status=$(http_status <<< "$out")
+    log "Received HTTP $status"
+
+    if [ "$status" != "200" ]; then
+        echo -e "HTTP/1.1 502 Bad Gateway\r\n\r\n"
+        return 1
+    fi
+
+    sed "1 s/^HTTP\/2.*$/HTTP\/1\.1 200 OK/" <<< "$out"
+}
+
+function http_status {
+    line="$(head -n 1)"
+    grep "HTTP" <<< "$line" > /dev/null
+    cut -d " " -f 2 <<< "$line"
 }
 
 function listen {
@@ -36,21 +54,18 @@ function listen {
 
     trap '[ -p $pipe ] && rm $pipe' EXIT
 
-    while true
-    do
+    while true; do
         # shellcheck disable=SC2094 # exception for named pipes
         handler < "$pipe" | nc -vl 0.0.0.0 "$PORT" > "$pipe"
     done
 }
 
 function main {
-    while true
-    do
+    while true; do
         listen || continue
     done
 }
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
-then
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     main "$@"
 fi
